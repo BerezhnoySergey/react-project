f9910c882bda571c7dadde6d55d38ba721722d60
sergey berezhnoy <berezhnoy.sergey550@gmail.com>
2022-10-31 00:14:27 +0200

    Add Footer Components

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,14 @@
 import "./Footer.scss";
 
 function Footer() {
-	return <div></div>;
+	return (
+		<div className="footer">
+			<h2 className="footer__header">Contacts</h2>
+			<p className="footer__text">
+				Want to know more or just chat? You are welcome!
+			</p>
+			<button className="footer__btn">Send message</button>
+		</div>
+	);
 }
 export default Footer;
